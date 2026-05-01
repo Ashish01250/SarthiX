57438afc1c33a4c448d44dd010d82acae3610220
Ashish kumar <115683719+Ashish01250@users.noreply.github.com>
2026-05-01 12:05:59 +0530

    supabase conection fix
    Updated Supabase client configuration to use service role key.

--- backend/src/config/supabaseClient.js
+++ backend/src/config/supabaseClient.js
@@ -4,7 +4,6 @@ dotenv.config();
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.VITE_SUPABASE_URL;
-// const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY;
 const supabaseKey = process.env.VITE_SUPABASE_SERVICE_ROLE_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
